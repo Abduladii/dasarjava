ddc7a6196d8fa394ec01b8086229e3b63ab16ea4
user <abduladi0709@gmail.com>
2022-03-14 19:24:41 +0700

    inisial project pertemuan 1 dasar java

---
+++ src/F_DefaultValue.java
@@ -0,0 +1,22 @@
+public class F_DefaultValue {
+    static boolean b;
+    static char c;
+    static byte bt;
+    static short s;
+    static short i;
+    static short l;
+    static short f;
+    static short d;
+
+    public static void main(String[] args) {
+        System.out.println("Default value b="+b);
+        System.out.println("Default value c="+c);
+        System.out.println("Default value bt="+bt);
+        System.out.println("Default value s="+s);
+        System.out.println("Default value i="+i);
+        System.out.println("Default value l="+l);
+        System.out.println("Default value f="+f);
+        System.out.println("Default value d="+d);
+    }
+
+}
