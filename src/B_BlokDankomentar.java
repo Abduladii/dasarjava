427947370591b5750032657b64372fd4258da384
user <abduladi0709@gmail.com>
2022-03-14 17:28:56 +0700

    inisial project pertemuan 1 dasar java

--- src/B_BlokDankomentar.java
+++ src/B_BlokDankomentar.java
@@ -1,4 +1,4 @@
-public class BlokDankomentar {
+public class B_BlokDankomentar {
     public static void main(String args[]){
         System.out.println("Pemrograman berorientasi objek dengan java");
     }
