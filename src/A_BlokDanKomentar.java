59c7ae1ba5daca48bc866fc224d19af2402c4a05
user <abduladi0709@gmail.com>
2022-03-14 15:34:13 +0700

    inisial project pertemuan 1 dasar java

--- src/A_BlokDanKomentar.java
+++ src/A_BlokDanKomentar.java
@@ -1,5 +1,14 @@
 public class A_BlokDanKomentar {
     public static void main(String[] args) {
         System.out.print("Dasar pemograman java ");
+        //Contoh konmentar satu baris
+
+        /*
+        Contoh komentar lebih dari satu baris
+        */
+
+        /**
+         * Java Doc-->baris ini untuk mendokumentasikan kode program
+         */
     }
 }
